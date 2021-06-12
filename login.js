5552a4da8761963913ccb1214dc7bf18f0259662
alicia kasemiire <aliciakasemiire@gmail.com>
2021-06-12 12:02:50 +0300

    simple fightclub registration site

---
+++ login.js
@@ -0,0 +1,14 @@
+//changing the behaviour of the html
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+
+//validation of the form
